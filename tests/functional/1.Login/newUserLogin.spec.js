48c14fd6cfe6775744eceab7b85aa3214b9fb50f
AshfaqSourav <ashfaqahmed3339@gmail.com>
2024-10-11 14:30:00 +0600

    git ignore update

--- tests/functional/1.Login/newUserLogin.spec.js
+++ tests/functional/1.Login/newUserLogin.spec.js
@@ -15,18 +15,18 @@ const MailSlurp = require('mailslurp-client').MailSlurp;
 
 test.describe("Login to Findit New User", () => {
     
-  test.beforeEach(async ({ page , loginPage  }) => {
+  test.beforeEach(async ({  loginPage  }) => {
     await loginPage.visit();
     await loginPage.acceptCookies();
 
   });
 
-  test("Successful New User Login", async ({ loginPage ,newUserLoginPage }) => {
-    const verificationCode=  await newUserLoginPage.generateNewEmailAndOtp(loginPage) 
-    await loginPage.loginEmailOtp(verificationCode);
-    await newUserLoginPage.newUserLoginDetails();
+  // test("Successful New User Login", async ({ loginPage ,newUserLoginPage }) => {
+  //   const verificationCode=  await newUserLoginPage.generateNewEmailAndOtp(loginPage) 
+  //   await loginPage.loginEmailOtp(verificationCode);
+  //   await newUserLoginPage.newUserLoginDetails();
 
-  });
+  // });
 
 
   // test("Empty State Full Name", async ({ loginPage , newUserLoginPage }) => {
@@ -34,11 +34,11 @@ test.describe("Login to Findit New User", () => {
   //   await loginPage.loginEmailOtp(verificationCode);
   //   await newUserLoginPage.verifyRequiredField(config.credentials.blankString);
   // });
-  // test ("Re-Upload Button check",async ({loginPage , newUserLoginPage })=>{
-  //   const verificationCode=  await newUserLoginPage.generateNewEmailAndOtp(loginPage) 
-  //   await loginPage.loginEmailOtp(verificationCode);
+  test ("Re-Upload Button check",async ({loginPage , newUserLoginPage })=>{
+    const verificationCode=  await newUserLoginPage.generateNewEmailAndOtp(loginPage) 
+    await loginPage.loginEmailOtp(verificationCode);
     
-  //   await newUserLoginPage.reUploadBtnCheck()
+    await newUserLoginPage.reUploadBtnCheck()
 
-  // })
+  })
 })
